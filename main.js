84aa610c3d7477af3e9a3e76269d97a2ebda570d
Deathlymad <meyerphilip96@googlemail.com>
2020-10-03 20:44:54 +0200

    implemented dirty, permanent walk circle

--- main.js
+++ main.js
@@ -10,21 +10,32 @@ import {loadLevel} from "./level.js"
 var lastTick = null;
 var unprocessed = 0;
 const FRAME_TIME = 1000/60;
-
+let frameCntr = 0
 
 function main() {
     initGraphics(document.getElementById('glCanvas'));
     initInput();
 
     // TODO: Change this
-    setPlayer(new GameObject("./Jabba.webp", vec2.fromValues(0, 0), vec2.fromValues(1, 1), "player"));
+    setPlayer(new GameObject("./assets/walk_circle.png", vec2.fromValues(0, 0), vec2.fromValues(1, 1), "player"));
     player.velocity = vec2.fromValues(0, 0);
     player.onGround = false;
+	player.sprite.texture.frames = 4
 
     window.running = true;
     requestAnimationFrame(update);
 
 	loadLevel(0, gl)
+}
+
+function updatePlayerAnimation()
+{
+	frameCntr += 1
+	if ((frameCntr % 15) === 0)
+	{
+		frameCntr = 0
+		player.sprite.texture.nextFrame()
+	}
 }
 
 function update(now) {
@@ -48,6 +59,8 @@ function update(now) {
         updateInput();
 		updatePhysics(FRAME_TIME / 1000);
 		updateView();
+		
+		updatePlayerAnimation();
     }
 
 	if (!level.isInitialized)
