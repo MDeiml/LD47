ec26d07d026239cb3c7dfd45438a4a15b4662792
Deathlymad <meyerphilip96@googlemail.com>
2020-10-04 19:18:37 +0200

    fixed spot lights

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function main() {
     initInput();
 
     initResource(function() {
-        loadLevel(1)
+        loadLevel(2)
 
         window.running = true;
         requestAnimationFrame(update);
