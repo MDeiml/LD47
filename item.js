d31c4719bd90ac74c3610cefa04cb122b725e7f1
Jakmannob <ga27hib@gmail.com>
2020-10-05 22:30:42 +0200

    Mapped more sound effects to items

--- item.js
+++ item.js
@@ -10,7 +10,6 @@ let ITEM_SPRITES = {
 	13 : "assets/lvl1/Pocket_Watch.png",
 	14 : "assets/lvl1/home-sweet-home.png",
 
-	// level 2 items
 	20 : "assets/lvl2/Ring_poliert_Blickdicht.png",
 	21 : "assets/lvl2/Diary_entry.png",
 	22 : "assets/lvl2/mailbox.png",
@@ -37,17 +36,32 @@ let ITEM_SPRITES = {
 
 export let ITEM_SOUNDS = {
     10 : "assets/sounds/paper/paper-10.wav",
-    11 : "assets/sounds/paper/paper-6.wav",
+    11 : "assets/sounds/paper/paper-7.wav",
     12 : "assets/sounds/paper/paper-45.wav",
     13 : "assets/sounds/clock/smallclock.wav",
 	14 : "assets/sounds/paper/paper-24.wav",
 	
-	// level 2 items
 	20 : "assets/sounds/ring/silent_ring.wav",
 	21 : "assets/sounds/paper/paper-34.wav",
 	// 22 : mailbox?
 	23 : "assets/sounds/paper/paper-43.wav",
-    24 : "assets/sounds/paper/paper-1.wav",
+	24 : "assets/sounds/paper/paper-1.wav",
+	
+	30 : "assets/sounds/paper/paper-22.wav",
+    31 : "assets/sounds/paper/paper-41.wav",
+    32 : "assets/sounds/paper/paper-42.wav",
+    33 : "assets/sounds/paper/paper-30.wav",
+    34 : "assets/sounds/paper/paper-37.wav",
+
+    // 40 : amber alert?
+    41 : "assets/sounds/ring/dark_ring.wav",
+    42 : "assets/sounds/paper/paper-38.wav",
+    43 : "assets/sounds/bottle/zisch-further-embiggened.wav",
+    // 44 : meds?
+
+    // 50 : empty bottles?
+    51 : "assets/sounds/paper/paper-33.wav",
+    // 52 : zippo?
 };
 
 let ITEM_TRIGGER = {
