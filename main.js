ed555814b7c289e61be4fcda57d66dd7982b8b6d
MDeiml <matthias@deiml.net>
2020-10-05 21:23:20 +0200

    Start with level 1

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function main() {
     initAudio();
 
     initResource(function() {
-        loadLevel(5)
+        loadLevel(1)
 
         window.running = true;
         requestAnimationFrame(update);
