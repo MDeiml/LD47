831b3ab8daaff21692c320d6061f0e449a850c57
MDeiml <matthias@deiml.net>
2020-10-05 21:20:13 +0200

    Fix level 4

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function main() {
     initAudio();
 
     initResource(function() {
-        loadLevel(1)
+        loadLevel(5)
 
         window.running = true;
         requestAnimationFrame(update);
